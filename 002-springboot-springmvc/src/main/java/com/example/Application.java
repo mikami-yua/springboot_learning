83815a094edc407acd62d182283c68030a4ec6d5
mikami-yua <lovejiaxi_1@163.com>
2021-03-18 16:24:16 +0800

    Initial commit

---
+++ 002-springboot-springmvc/src/main/java/com/example/Application.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//springboot启动入口类
+
+/**
+ * springboot项目代码必须载application类所在目录或下级目录
+ */
+@SpringBootApplication//开启springboot配置的注解
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
